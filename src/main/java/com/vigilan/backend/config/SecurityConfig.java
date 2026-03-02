628e33dd14fbd7beb6b224287ca126a44ab23169
Prapti Siva <praptisivaahis@gmail.com>
2026-03-02 19:00:03 +0530

    feat: Config

--- src/main/java/com/vigilan/backend/config/SecurityConfig.java
+++ src/main/java/com/vigilan/backend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://13.234.32.80"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
